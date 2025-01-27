067edfc86e4f329bbb047cb0594f5053bdbd14af
Parikaragarwal <parikaragarwal@gmail.com>
2025-01-27 14:06:35 +0530

    Sign Up Page->2 new buttons

---
+++ public/js/signIn.js
@@ -0,0 +1,8 @@
+document.getElementById('redirectSignUp').addEventListener('click', () => {
+    window.location.href = '/user/signUp'; // Redirect to the Sign-Up page
+  });
+  
+  document.getElementById('redirectHome').addEventListener('click', () => {
+    window.location.href = '/'; // Redirect to the Home page
+  });
+  

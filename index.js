bfb54b3240c139e457b4cb5f1d7d26a85b1a2c6a
Parikaragarwal <parikaragarwal@gmail.com>
2025-01-31 00:22:29 +0530

    Added Multer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const multer= require("multer");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
 const methodOverride = require("method-override");
